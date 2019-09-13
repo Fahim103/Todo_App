1734310715dcf16a848bf350ca056bf82428648a
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2019-09-13 23:36:11 +0600

    Can filter todos using drop down list

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ $(document).ready(function() {
     // ADD NEW TODO TO THE TABLE
     btnAddNewTodo.on('click', function(){
         let newTodoToBeAdded = txtNewTodo.val();
-        if(newTodoToBeAdded == "")
+        if(newTodoToBeAdded == ""){
+            alert("Enter a todo");
+            txtNewTodo.focus();
             return;
+        }
         let currentTodoID = uniqueID();
 
         $('#tableBody').append('<tr>');
@@ -28,6 +31,8 @@ $(document).ready(function() {
         txtNewTodo.val("");
         // Set focus on the ADD TODO input field again
         txtNewTodo.focus();
+
+        showTodoFilter();
     });
 
    
@@ -36,6 +41,8 @@ $(document).ready(function() {
     $('#btnUpdateTodo').on('click', updateTodo);
 
     $('#btnCancelUpdateTodo').on('click', cancelUpdateTodo);
+
+    $('#showTodoFilter').on('change', showTodoFilter);
 
 });
 
@@ -50,8 +57,10 @@ let uniqueID = (function() {
 function isTodoCompleted(){
     if($(this).is(":checked")) {
         $(this).parent().prev().css({"text-decoration": "line-through"});
+        showTodoFilter()
     }else{
         $(this).parent().prev().css({"text-decoration": "none"});
+        showTodoFilter()
     }
 }
 
@@ -115,6 +124,7 @@ function disableInputsExceptUpdateDiv(){
     $('.btnEdit').attr("disabled", "disabled");
     $('.btnDelete').attr("disabled", "disabled");
     $(':checkbox').attr("disabled","disabled");
+    $('#showTodoFilter').prop("disabled", true);
 }
 
 function enableInputs() { 
@@ -123,4 +133,42 @@ function enableInputs() {
     $('.btnEdit').removeAttr("disabled");
     $('.btnDelete').removeAttr("disabled");
     $(':checkbox').removeAttr("disabled");
+    $('#showTodoFilter').prop("disabled", false);
+
+}
+
+// Filter Todo according to the drop down and show them in the table
+function showTodoFilter(){
+    let dropDownValue = $('#showTodoFilter').val();
+
+    let checkboxArray = $('#tableBody').children('tr').children('td').children(':checkbox');
+
+    if(dropDownValue === "completed"){
+        // Show Completed TODOS
+        checkboxArray.each(function () { 
+            if($(this).is(':checked')){
+                $(this).parent().parent().removeClass('hiddenInput');
+            }else{
+                $(this).parent().parent().addClass('hiddenInput');
+            }
+        });
+    }else if(dropDownValue === "incomplete"){
+        // Show Incomplete TODOS
+        checkboxArray.each(function () { 
+            if($(this).is(':checked')){
+                $(this).parent().parent().addClass('hiddenInput');
+            }else{
+                $(this).parent().parent().removeClass('hiddenInput');
+            }
+        });
+    }else{
+        // Show All TODOS
+        checkboxArray.each(function () { 
+            if($(this).is(':checked')){
+                $(this).parent().parent().removeClass('hiddenInput');
+            }else{
+                $(this).parent().parent().removeClass('hiddenInput');
+            }
+        });
+    } 
 }
